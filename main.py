8f51e293cccac313034880209067947fdad2c47b
eagui041 <eagui041@uottawa.ca>
2023-11-14 15:31:11 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# SECTION - Import necessary modules
+
+
+# SECTION - Define functions
+
+# Calibration Function
+def calibrate():
+    
+# Zitai's routine for calibration?
+
+# SECTION - Define main function
+
+
+
+# SECTION - Call main function
+if __name__ == "__main__":
+    main()
