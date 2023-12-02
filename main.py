3b2bc2126d16c7ef5e54c47bf11d9b1a746e6fa1
eagui041 <eagui041@uottawa.ca>
2023-12-02 13:10:39 -0500

    Calibration Setup, Servo Initialization, UnitTest

--- main.py
+++ main.py
@@ -1,65 +1,55 @@
 # SECTION - Import necessary modules
-import time 
+import time
 import math
 from classes import Pin, PWM, ADC
 
-#Prints welcome to the game: 
+# Game introduction and calibration setup:
 print("welcome to the etch a sketch game")
 print("Use the right potentiometer to control Y and the left potentiometer to control X")
 print("Use the middle button to raise and lower the pen")
+time.sleep(5)
+offset_shoulder = input("Enter the offset for the shoulder servo motor: ")
+offset_elbow = input("Enter the offset for the elbow servo motor: ")
+time.sleep(5)
+print("Calibration complete")
 
-# SECTION - Define global variables
+# SECTION - GLOBAL VARIABLES
+
+# Constants for PWM range
+PWM_MIN = INSERT_MIN_PWM_VALUE_HERE
+PWM_MAX = INSERT_MAX_PWM_VALUE_HERE
+
+# Initialize the servo motors
+shoulder_servo = PWM(Pin(INSERT_PIN_NUMBER_HERE))
+elbow_servo = PWM(Pin(INSERT_PIN_NUMBER_HERE))
+pen_servo = PWM(Pin(INSERT_PIN_NUMBER_HERE))
+
 
 def setUpPotPins():
-    right_potentiometer = ADC(0)
-    left_potentiometer = ADC(1)
+    right_potentiometer = ADC(Pin(26))
+    left_potentiometer = ADC(Pin(27))
     return left_potentiometer, right_potentiometer
 
-# SECTION - Define functions
+# SECTION - FUNCTIONS
 
-def readRLInput(left_potentiometer,right_potentiometer):       
+# Calibration offset function
+def run_calibration(offset_shoulder, offset_elbow):
+    shoulder_angle, elbow_angle = inverse_kinematics(0, 0)
+    servo_shoulder_offset = shoulder_angle - offset_shoulder
+    servo_elbow_offset = offset_elbow - elbow_angle
+    return servo_shoulder_offset, servo_elbow_offset
+
+# This function reads the input from the potentiometers
+def readRLInput(left_potentiometer, right_potentiometer):
     right_val = right_potentiometer.read_u16()
     left_val = left_potentiometer.read_u16()
-
     return right_val, left_val
 
-left_poten,right_poten = setUpPotPins()
-while True:
-
-    right,left = readRLInput(left_poten,right_poten)
-    print(right,left)
-    time.sleep(0.1) #100ms delay
-    
 
 
-# SECTION - Define functions
-
-#def move_shoulder(angle):
-    #Code to control the shoulder servo motor 
-
-#def move_elbow(angle):
-    #Code to control the elbow servo motor
-
-#def raise_lower_pen(is_pen_down):
-    #Code to raise or lower the pen servo motor 
-    #is_pen_down could be a boolean variable where True means the pen is down and False means the pen in up.
-
-#def get_x_potentiometer_value():
-    #Code to read input for X-axis
-    #ADC to read analog value from potentiometer
-
-#def get_y_potentiometer_value():
-    #Code to read input for Y-axis
-    #ADC to read analog value from potentiometer
-
-# Calibration Function
-#def calibrate():
-
-    
 # Inverse Kinematics Function
 def inverse_kinematics(Cx, Cy):
-    #Code to calculate the angle for the shoulder and elbow servo motors
-    #return shoulder_angle, elbow_angle
+    # Code to calculate the angle for the shoulder and elbow servo motors
     Ax = -50
     Ay = 139.5
     La = 155
@@ -69,11 +59,29 @@ def inverse_kinematics(Cx, Cy):
     BAC = math.acos((La**2 + AC**2 - Lb**2)/(2*La*AC))
     ACB = math.asin((La*math.sin(BAC))/Lb)
     YAC = math.acos((Ay**2 + AC**2 - AbaseC**2)/(2*Ay*AC))
-    alpha = math.degrees(BAC + YAC)
-    beta = math.degrees(BAC + ACB)
-    return alpha, beta
+    alpha_angle = math.degrees(BAC + YAC)
+    beta_angle = math.degrees(BAC + ACB)
+    return alpha_angle, beta_angle
 
-# Calibration offset
-shoulder_angle, elbow_angle = inverse_kinematics(0, 0)
-servo_shoulder_offset = shoulder_angle - 75
-servo_elbow_offset = 150- elbow_angle
+# Function to control the shoulder servo motor
+# def move_shoulder(angle):
+
+
+# Function to control the elbow servo motor
+# def move_elbow(angle):
+
+# def raise_lower_pen(is_pen_down):
+# Code to control the elbow servo motor
+
+
+
+# SECTION - MAIN PROGRAM
+servo_shoulder_offset, servo_elbow_offset = run_calibration()
+left_poten, right_poten = setUpPotPins()
+
+while True:
+    right, left = readRLInput(left_poten, right_poten)
+    shoulder_angle, elbow_angle = inverse_kinematics(left, right)
+    move_shoulder(shoulder_angle + servo_shoulder_offset)
+    move_elbow(elbow_angle + servo_elbow_offset)
+    
---
+++ test_main.py
@@ -0,0 +1,21 @@
+import unittest
+from main import setUpPotPins, readRLInput
+
+class TestMain(unittest.TestCase):
+    def test_setUpPotPins(self):
+        left_potentiometer, right_potentiometer = setUpPotPins()
+        self.assertEqual(left_potentiometer, 1)
+        self.assertEqual(right_potentiometer, 0)
+    
+    def test_readRLInput(self):
+        right_val, left_val = readRLInput(1,0)
+        self.assertEqual(right_val, 0)
+        self.assertEqual(left_val, 1)
+
+    def test_inverse_kinematics(self):
+        self.assertEqual(1, 1)
+
+    def test_run_calibration(self):
+        self.assertEqual(1, 1)
+
+    def test
