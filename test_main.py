297cb3b1274fbc62ffea3d8735a9cb39ab644908
eagui041 <eagui041@uottawa.ca>
2023-12-05 14:01:33 -0500

    test updates

--- test_main.py
+++ test_main.py
@@ -3,10 +3,5 @@ from main import readRLInput, inverse_kinematics
 
 class TestMain(unittest.TestCase):
     
-    def test_readRLInput(self):
-        right_val, left_val = readRLInput(1,0)
-        self.assertEqual(right_val, 0)
-        self.assertEqual(left_val, 1)
-
     def test_inverse_kinematics(self):
         self.assertEqual((500,500), (82.3485, 123.4872))
